2b2614a76175a7160dd6dec8d7a92ecacbb1c3df
ki7mt <ki7mt@yahoo.com>
2018-01-09 15:49:35 -0700

    Add shell project for postinstall

---
+++ src/JTSDK.NetCore/JTSDK.Core.PostInstall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JTSDK.Core.PostInstall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
