16a50f3342cb71c821d74fcd90cfcb3f16a048b1
ki7mt <ki7mt@yahoo.com>
2018-06-02 02:05:05 -0600

    More updates to JTSDK-Maint

--- src/JTSDK.Win32/root/jtsdk-maint-env.cmd
+++ src/JTSDK.Win32/root/jtsdk-maint-env.cmd
@@ -26,15 +26,10 @@
 @ECHO OFF
 SET URL=https://github.com/KI7MT/jtsdk-dotnet-core.git
 SET JTSDK_HOME=%CD%
-SET pgitv=2.17.1.2
-
 
 :: .Net Version at time fo initial release
 IF EXIST %CD%\ver.jtsdk (
 SET /P ver_jtsdk=<ver.jtsdk
-)
-IF EXIST %CD%\ver.dotnet (
-SET /P ver_dotnet=/P version=<ver.dotnet
 )
 
 :: Set some basic ennvironment variables
@@ -42,36 +37,34 @@ COLOR 0E
 SET LANG=en_US
 
 :: Set Minimal Path
-SET git_dir=%JTSDK_HOME%\tools\pgit\%pgitv%\cmd
-SET dotnet_dir=%JTSDK_HOME%\tools\dotnet\%ver_dotnet%
 SET unix_tools=%JTSDK_HOME%\tools\msys2\usr\bin
 SET srcd=%JTSDK_HOME%\src
 
 :: Set Final path
-SET PATH=%JTSDK_HOME%;%git_dir%;%dotnet_dir%;%unix_tools%;%srcd%;%WINDIR%\System32
+SET PATH=%JTSDK_HOME%;%PATH%;%unix_tools%
 
 :: Make sure srcd exists
 
 :: Full paths are needed for some versions of Windows
 :: Why some are and others are not, is still unknown.
-SET version=%version% %rev%
 TITLE JTSDK Maintenance Console
 
 :: Doskey's for use with the Maintenance Environment
 DOSKEY help-checkout=@echo This feature has not be implemented yet.
 DOSKEY help-install=@echo This feature has not be implemented yet.
 DOSKEY help-upgrade=@echo This feature has not be implemented yet.
-DOSKEY clone-jtsdk=git clone %URL% jtsdk-master
-DOSKEY update-jtsdk=git pull
+DOSKEY clone-jtsdk=cd %srcd% $T start /wait git clone %URL% jtsdk-master
+DOSKEY update-jtsdk=cd %srcd%^\jtsdk-master $T start /wait git pull
 DOSKEY clear=cls
 DOSKEY ls=dir
 
 :: Start Main Script
 CD /D %JTSDK_HOME%
 IF NOT EXIST %srcd% (mkdir %srcd%)
+dotnet --version |%unix_tools%\awk.exe "{print $1}" >d.v. & set /p ver_dotnet=<d.v & rm d.v
 CLS
 ECHO -----------------------------------------------------------
-ECHO  JTSDK Maintenance Environment - %version%
+ECHO  JTSDK Maintenance Environment - %ver_jtsdk%
 ECHO -----------------------------------------------------------
 ECHO.
 ECHO  JTSDK Version .........: %ver_jtsdk%
