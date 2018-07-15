44abe90ea8e75c0d11c92a555bfb9d960716e637
ki7mt <ki7mt@yahoo.com>
2018-07-15 03:19:36 -0600

    Update JTSDN Env verson list in main script

--- src/JTSDK.Win32/root/jtsdk-env.cmd
+++ src/JTSDK.Win32/root/jtsdk-env.cmd
@@ -246,13 +246,14 @@ pkg-config --version >p.c & SET /P PKG=<p.c & rm p.c
 git --version |awk "{print $3}" >c.m & SET /P GITV=<c.m & rm c.m
 svn --version |awk "{print $3}" >c.m & SET /P SVNV=<c.m & rm c.m
 mvn --version |awk "FNR==1 {print $3}" >mv.v & SET /P MVNV=<mv.v & rm mv.v
-gradle --version |awk "FNR==2 {print $2}" >g.v & SET /P GDLV=<g.v & rm g.v
+gradle --version |awk "FNR==3 {print $2}" >g.v & SET /P GDLV=<g.v & rm g.v
 sqlite3 --version |awk "FNR==1 {print $1}" >s.v & SET /P SQ3V=<s.v & rm s.v
 dotnet --version >d.n & SET /P DOTV=<d.n & rm d.n
 bash --version |awk "FNR==1 {print $4}" >b.v & SET /P BENV=<b.v & rm b.v
 pandoc --version | awk "FNR==1 {print $2}" >p.v & SET /P PDV=<p.v & rm p.v
 docfx.exe --version | awk "FNR==1 {print $2}" >p.v & SET /P DFX=<p.v & rm p.v
 nuget help |awk "FNR==1 {print $3}" >ng.v & SET /P NUG=<ng.v & rm ng.v
+javac -version |awk "FNR==1 {print $2}" >j.v & SET /P JAV=<j.v & rm j.v
 
 ::------------------------------------------------------------------------------
 :: CONDITIONAL VERSION CHECKS
@@ -275,13 +276,11 @@ ECHO    Version        : %version%
 ECHO    Git Tag        : %git_tag%
 ECHO.
 ECHO  Core Tools
-ECHO    Asciidoctor    : %ADV%
 ECHO    Cmake          : %CMV%
-ECHO    Git            : %GITV%
 ECHO    DotNet Core    : %DOTV%
 ECHO    GCC            : %CVER%
+ECHO    Git            : %GITV%
 ECHO    InnoSetup      : %innov%
-ECHO    Maven          : %MVNV%
 ECHO    Mingw32 Make   : %GNMK%
 ECHO    MSYS2 Bash     : %BENV%
 ECHO    NSIS           : %NSM%
@@ -289,15 +288,21 @@ ECHO    NuGet          : %NUG%
 ECHO    Pkg Config     : %PKG%
 ECHO    Qmake          : %QMV%
 ECHO    QT Version     : %QTV%
-ECHO    SQLite         : %SQ3V%
 ECHO    Subversion     : %SVNV%
 ECHO.
+ECHO. Java Tools
+ECHO    Java           : %JAV%
+ECHO    Gradle         : %GDLV%
+ECHO    Maven          : %MVNV%
+ECHO.
 ECHO. Doc Tools
+ECHO    Asciidoctor    : %ADV%
 ECHO    DocFX          : %DFX%
 ECHO    Pandoc         : %PDV%
 ECHO.
 ECHO. Database Tools
 ECHO    PostgeSQL      : %PGV%
+ECHO    SQLite         : %SQ3V%
 ECHO.
 ENDLOCAL 
 
