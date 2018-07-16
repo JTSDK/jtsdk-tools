61ec7d00bffdbb079374c6395f42e38ea5fd4ad9
ki7mt <ki7mt@yahoo.com>
2018-07-15 20:16:10 -0600

    Workload check now funcitonal

--- src/JTSDK.Win32/root/jtsdk-env.cmd
+++ src/JTSDK.Win32/root/jtsdk-env.cmd
@@ -27,11 +27,11 @@
 SET LANG=en_US
 
 :: RESET TOOL STATUS
-SET DBTOOLS=
-SET DOCTOOLS=
-SET JAVATOOLS=
-SET PYTOOLS=
-SET CORETOOLS=
+SET DBTOOLS=Not Installed
+SET DOCTOOLS=Not installed
+SET JAVATOOLS=Not Installed
+SET PYTOOLS=Not Installed
+SET CORETOOLS=Not Installed
 
 :: JTSDK Version
 SET /P version=<ver.jtsdk
@@ -68,215 +68,227 @@ mkdir %JTSDK_HOME%\tools\hamlib >NUL 2>&1
 :: Set those tool paths ONLY if they were installed with jtsdk-db-tools
 ::------------------------------------------------------------------------------
 
-IF EXIST "%JTSDK_HOME%\doc.tools" (
-    SET DOCTOOLS=Installed
-    SET asciidocv=8.6.9
-    SET docfxv=2.37
-    SET pandocv=2.2.1
+:: CHeck doc-tools
+IF EXIST "%JTSDK_HOME%\doc.tools" (GOTO DOC_TOOLS)
+GOTO DB_CHECK
 
-    :: AsciiDoc
-    SET asciidoc_dir=%JTSDK_HOME%\tools\asciidoc\%asciidocv%
-    SET "asciidoc_dir_f=%asciidoc_dir:\=/%"
-    SET JTSDK_PATH=%asciidoc_dir%
+:DOC_TOOLS
+ECHO ^*^* Setting Doc Tools Paths
+SET DOCTOOLS=Installed
+SET asciidocv=8.6.9
+SET docfxv=2.37
+SET pandocv=2.2.1
 
-    :: DOCFX
-    SET docfx_dir=%JTSDK_HOME%\tools\docfx\%docfxv%
-    SET JTSDK_PATH=%JTSDK_PATH%;%docfx_dir%
+:: AsciiDoc
+SET asciidoc_dir=%JTSDK_HOME%\tools\asciidoc\%asciidocv%
+SET "asciidoc_dir_f=%asciidoc_dir:\=/%"
+SET JTSDK_PATH=%asciidoc_dir%
 
-    :: PANDOC
-    SET pandoc_dir=%JTSDK_HOME%\tools\pandoc\%pandocv%
-    SET JTSDK_PATH=%JTSDK_PATH%;%pandoc_dir%
+:: DOCFX
+SET docfx_dir=%JTSDK_HOME%\tools\docfx\%docfxv%
+SET JTSDK_PATH=%JTSDK_PATH%;%docfx_dir%
 
-) ELSE (
+:: PANDOC
+SET pandoc_dir=%JTSDK_HOME%\tools\pandoc\%pandocv%
+SET JTSDK_PATH=%JTSDK_PATH%;%pandoc_dir%
 
-    SET DOCTOOLS=Not Installed
-
-)
-:: END DOC TOOLS
+:: END DOC TOOLS ---------------------------------------------------------------
+GOTO DB_CHECK
 
 ::------------------------------------------------------------------------------
 :: DB TOOLS PATH
 :: Set those tool paths ONLY if they were installed with jtsdk-db-tools
 ::------------------------------------------------------------------------------
 
-IF EXIST "%JTSDK_HOME%\db.tools" (
+:DB_CHECK
+IF EXIST %JTSDK_HOME%\db.tools (GOTO DB_TOOLS)
+GOTO JAVA_CHECK
 
-    SET DBTOOLS=Installed
-    SET sqlite3v=3.24.0
-    SET psqlv=10
+:DB_TOOLS
+ECHO ^*^* Setting DB Tool Paths
+SET DBTOOLS=Installed
+SET sqlite3v=3.24.0
+SET psqlv=10
 
-    :: SQLITE
-    SET sqlite3_dir=%JTSDK_HOME%\tools\sqlite3\%sqlite3v%
-    SET JTSDK_PATH=%JTSDK_PATH%;%sqlite3_dir%
+:: SQLITE
+SET sqlite3_dir=%JTSDK_HOME%\tools\sqlite3\%sqlite3v%
+SET JTSDK_PATH=%JTSDK_PATH%;%sqlite3_dir%
 
-    :: POSTGRES
-    IF EXIST "%JTSDK_HOME%\tools\PostgreSQL\10\pg_env.bat" (
-    call %JTSDK_HOME%\tools\PostgreSQL\10\pg_env.bat
-    )
-
-) ELSE (
-
-    SET DBTOOLS=Not Installed
+:: POSTGRES
+IF EXIST "%JTSDK_HOME%\tools\PostgreSQL\10\pg_env.bat" (
+call %JTSDK_HOME%\tools\PostgreSQL\10\pg_env.bat
 )
-:: END DB TOOLS
+
+:: END DB TOOLS ----------------------------------------------------------------
+GOTO JAVA_CHECK
 
 ::------------------------------------------------------------------------------
 :: JAVA TOOLS
 :: Set those tool paths ONLY if they were installed with jtsdk-java-tools
 ::------------------------------------------------------------------------------
-IF EXIST "%JTSDK_HOME%\java.tools" (
 
-    SET JAVATOOLS=Installed
-    SET gradlev=4.8.1
-    SET mavenv=3.5.4
+:JAVA_CHECK
+IF EXIST "%JTSDK_HOME%\java.tools" (GOTO JAVA_TOOLS)
+GOTO CORE_CHECK
 
-    :: GRADLE
-    SET gradle_dir=%JTSDK_HOME%\tools\gradle\%gradlev%
-    SET JTSDK_PATH=%JTSDK_PATH%;%gradle_dir%\bin
+:JAVA_TOOLS
+ECHO ^*^* Setting Java Tool Paths
+SET JAVATOOLS=Installed
+SET gradlev=4.8.1
+SET mavenv=3.5.4
 
-    :: MAVEN
-    SET maven_dir=%JTSDK_HOME%\tools\maven\%mavenv%
-    SET JTSDK_PATH=%JTSDK_PATH%;%maven_dir%\bin
+:: GRADLE
+SET gradle_dir=%JTSDK_HOME%\tools\gradle\%gradlev%
+SET JTSDK_PATH=%JTSDK_PATH%;%gradle_dir%\bin
 
-) ELSE (
+:: MAVEN
+SET maven_dir=%JTSDK_HOME%\tools\maven\%mavenv%
+SET JTSDK_PATH=%JTSDK_PATH%;%maven_dir%\bin
 
-    SET JAVATOOLS=Not Installed
-)
-:: END JAVA TOOLS
+:: END JAVA TOOLS --------------------------------------------------------------
+GOTO CORE_CHECK
 
 ::------------------------------------------------------------------------------
 :: CORE TOOLS
 :: Set those tool paths ONLY if they were installed with jtsdk-core-tools
 ::------------------------------------------------------------------------------
 
-IF EXIST "%JTSDK_HOME%\core.tools" (
+:CORE_CHECK
+IF EXIST "%JTSDK_HOME%\core.tools" (GOTO CORE_TOOLS)
+GOTO PY_CHECK
 
-    SET CORETOOLS=Installed
-    SET fftwv=3.3.5
-    SET innov=5.5.9
-    SET libusbv=1.0.22
-    SET nsisv=3.03
-    SET pkgconfigv=0.28.1
-    SET rubyv=2.5.1-2
-    SET svnv=1.9.7
+:CORE_TOOLS
+ECHO ^*^* Setting Core Tool Paths
+SET CORETOOLS=Installed
+SET fftwv=3.3.5
+SET innov=5.5.9
+SET libusbv=1.0.22
+SET nsisv=3.03
+SET pkgconfigv=0.28.1
+SET rubyv=2.5.1-2
+SET svnv=1.9.7
 
-    :: FFTW
-    SET fftw3f_dir=%JTSDK_HOME%\tools\fftw\%fftwv%
-    SET "fftw3f_dir_f=%fftw3f_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%fftw3f_dir%
+:: FFTW
+SET fftw3f_dir=%JTSDK_HOME%\tools\fftw\%fftwv%
+SET "fftw3f_dir_f=%fftw3f_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%fftw3f_dir%
 
-    :: INNO
-    SET inno_dir=%JTSDK_HOME%\tools\inno\%innov%
-    SET "inno_dir_f=%inno_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%inno_dir%
+:: INNO
+SET inno_dir=%JTSDK_HOME%\tools\inno\%innov%
+SET "inno_dir_f=%inno_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%inno_dir%
 
-    :: LIBUSB
-    SET libusb_dir=%JTSDK_HOME%\tools\libusb\%libusbv%
-    SET "libusb_dir_f=%libusb_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%linusb_dir%
+:: LIBUSB
+SET libusb_dir=%JTSDK_HOME%\tools\libusb\%libusbv%
+SET "libusb_dir_f=%libusb_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%linusb_dir%
 
-    :: NSIS
-    SET nsis_dir=%JTSDK_HOME%\tools\nsis\%nsisv%
-    SET "nsis_dir_f=%nsis_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%nsis_dir%
+:: NSIS
+SET nsis_dir=%JTSDK_HOME%\tools\nsis\%nsisv%
+SET "nsis_dir_f=%nsis_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%nsis_dir%
 
-    :: NUGET
-    SET nuget_dir=%JTSDK_HOME%\tools\nuget
-    SET JTSDK_PATH=%JTSDK_PATH%;%nuget_dir%
+:: NUGET
+SET nuget_dir=%JTSDK_HOME%\tools\nuget
+SET JTSDK_PATH=%JTSDK_PATH%;%nuget_dir%
 
-    :: PKG_CONFIG
-    SET pkgconfig_dir=%JTSDK_HOME%\tools\pkgconfig\%pkgconfigv%\bin
-    SET "pkgconfig_dir_f=%pkgconfig_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%pkgconfig_dir%
+:: PKG_CONFIG
+SET pkgconfig_dir=%JTSDK_HOME%\tools\pkgconfig\%pkgconfigv%\bin
+SET "pkgconfig_dir_f=%pkgconfig_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%pkgconfig_dir%
 
-    :: RUBY
-    SET ruby_dir=%JTSDK_HOME%\tools\ruby\%rubyv%\bin
-    SET "ruby_dir_f=%ruby_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%ruby_dir%
+:: RUBY
+SET ruby_dir=%JTSDK_HOME%\tools\ruby\%rubyv%\bin
+SET "ruby_dir_f=%ruby_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%ruby_dir%
 
-    :: SVN
-    SET svn_dir=%JTSDK_HOME%\tools\subversion\%svnv%\bin
-    SET "svn_dir_f=%svn_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%svn_dir%
+:: SVN
+SET svn_dir=%JTSDK_HOME%\tools\subversion\%svnv%\bin
+SET "svn_dir_f=%svn_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%svn_dir%
 
-    :: UNIX TOOLS
-    SET unix_dir=%JTSDK_HOME%\tools\msys2\usr\bin
-    SET JTSDK_PATH=%JTSDK_PATH%;%unix_dir%
+:: UNIX TOOLS
+SET unix_dir=%JTSDK_HOME%\tools\msys2\usr\bin
+SET JTSDK_PATH=%JTSDK_PATH%;%unix_dir%
 
-    ::------------------------------------------------------------------------------
-    :: CONDITIONAL PATHS for Multiple versions of Cmake
-    ::------------------------------------------------------------------------------
+::------------------------------------------------------------------------------
+:: CONDITIONAL PATHS for Multiple versions of Cmake
+::------------------------------------------------------------------------------
 
-    :: Note: Cmake 3.10.3 was chosen to match Ubuntu Bionc (18.04.0)
-    ::       if the first point release ugrades this version, JTSDK should
-    ::       follow suit.
+:: Note: Cmake 3.10.3 was chosen to match Ubuntu Bionc (18.04.0)
+::       if the first point release ugrades this version, JTSDK should
+::       follow suit.
 
-    IF EXIST "%JTSDK_CONFIG%\cmake310" (
-    SET cmakev=3.10.3
-    ) ELSE (
-    SET cmakev=3.5.2
-    )
-    SET cmake_dir=%JTSDK_HOME%\tools\cmake\%cmakev%\Bin
-    SET "cmake_dir_f=%cmake_dir:\=/%"
-    SET JTSDK_PATH=%JTSDK_PATH%;%cmake_dir%
-
-    ::------------------------------------------------------------------------------
-    :: CONDITIONAL PATHS for Multiple versions of Qt
-    ::------------------------------------------------------------------------------
-    IF EXIST "%JTSDK_CONFIG%\QT59" (
-    SET QTV=5.9.5
-    SET PROMPT=$C QT 5.9 $F $P^:
-    SET title-string=JTSDK-Tools QT 5.9 Environment
-    SET QTD=%JTSDK_HOME%\tools\Qt\5.9.5\mingw53_32\bin
-    SET QTP=%JTSDK_HOME%\tools\Qt\5.9.5\mingw53_32\plugins\platforms
-    SET GCCD=%JTSDK_HOME%\tools\Qt\Tools\mingw530_32\bin
-    ) ELSE (
-    set QTV=5.5
-    SET PROMPT=$C QT 5.5 $F $P^:
-    SET title-string=JTSDK-Tools QT 5.5 Environment
-    SET QTD=%JTSDK_HOME%\tools\Qt\5.5\mingw492_32\bin
-    SET QTP=%JTSDK_HOME%\tools\Qt\5.5\mingw492_32\plugins\platforms
-    SET GCCD=%JTSDK_HOME%\tools\Qt\Tools\mingw492_32\bin
-    )
-    SET JTSDK_PATH=%GCCD%;%QTD%;%QTP%;%JTSDK_PATH%
-
-    :: Set forward slash paths for MSYS2 and QT
-    SET "QTD_F=%QTD:\=/%"
-    SET "GCCD_F=%GCCD:\=/%"
-
+IF EXIST "%JTSDK_CONFIG%\cmake310" (
+SET cmakev=3.10.3
 ) ELSE (
-
-    SET CORETOOLS=Not Installed
+SET cmakev=3.5.2
 )
-:: END CORE TOOLS
+SET cmake_dir=%JTSDK_HOME%\tools\cmake\%cmakev%\Bin
+SET "cmake_dir_f=%cmake_dir:\=/%"
+SET JTSDK_PATH=%JTSDK_PATH%;%cmake_dir%
+
+::------------------------------------------------------------------------------
+:: CONDITIONAL PATHS for Multiple versions of Qt
+::------------------------------------------------------------------------------
+IF EXIST %JTSDK_CONFIG%\QT59 (
+SET QTV=5.9.5
+SET PROMPT=$CQT-5.9$F $P ^>
+SET title-string=JTSDK-Tools QT 5.9 Environment
+SET QTD=%JTSDK_HOME%\tools\Qt\5.9.5\mingw53_32\bin
+SET QTP=%JTSDK_HOME%\tools\Qt\5.9.5\mingw53_32\plugins\platforms
+SET GCCD=%JTSDK_HOME%\tools\Qt\Tools\mingw530_32\bin
+) ELSE (
+set QTV=5.5
+SET PROMPT=$CQT-5.5$F $P ^>
+SET title-string=JTSDK-Tools QT 5.5 Environment
+SET QTD=%JTSDK_HOME%\tools\Qt\5.5\mingw492_32\bin
+SET QTP=%JTSDK_HOME%\tools\Qt\5.5\mingw492_32\plugins\platforms
+SET GCCD=%JTSDK_HOME%\tools\Qt\Tools\mingw492_32\bin
+)
+SET JTSDK_PATH=%GCCD%;%QTD%;%QTP%;%JTSDK_PATH%
+
+:: Set forward slash paths for MSYS2 and QT
+SET "QTD_F=%QTD:\=/%"
+SET "GCCD_F=%GCCD:\=/%"
+
+:: END CORE TOOLS -------------------------------------------------------------
+GOTO PY_CHECK
 
 ::------------------------------------------------------------------------------
 :: PYTHON TOOLS
 :: Set those tool paths ONLY if they were installed with jtsdk-core-tools
 ::------------------------------------------------------------------------------
-IF EXIST "%JTSDK_CONFIG%\Anaconda3\Scripts\activate.bat" (
 
-    SET PYTOOLS=Installed
-    call %JTSDK_HOME%\tools\Anaconda3\Scripts\activate.bat D:\JTSDK-Tools\tools\Anaconda3
+:PY_CHECK
+IF EXIST "%JTSDK_HOME%\tools\Anaconda3\Scripts\activate.bat" (GOTO PY_TOOLS)
+GOTO SET_FINAL 
 
-) ELSE (
-    SET PYTOOLS=Not Installed
-)
+:PY_TOOLS
+ECHO ^*^* Setting Python Tool Paths
+SET PYTOOLS=Installed
+call %JTSDK_HOME%\tools\Anaconda3\Scripts\activate.bat D:\JTSDK-Tools\tools\Anaconda3
+GOTO SET_FINAL
 
 ::------------------------------------------------------------------------------
 :: SET FINAL ENVIRONMENT PATHS and CONSOLE TITLE
 ::------------------------------------------------------------------------------
+
+:SET_FINAL
+ECHO ^*^* Set Final Paths and headers
 TITLE %title-string%
 SET PATH=%JTSDK_PATH%;%PATH%
 
 ::------------------------------------------------------------------------------
 :: GENERATE DOSKEY's
 ::------------------------------------------------------------------------------
+ECHO ^*^* Generating Doskey^'s
 call %JTSDK_HOME%\scripts\cmd\utils\gen-doskey.cmd
 
 ::------------------------------------------------------------------------------
 :: AUTO GENERATE QT Tool Chain Files (safety feature)
 ::------------------------------------------------------------------------------
+ECHO ^*^* Generating Generating Tool Chain Files
 call %JTSDK_HOME%\scripts\cmd\utils\qt-gen-tc.cmd >NUL 2>&1
 
 ::------------------------------------------------------------------------------
@@ -296,7 +308,7 @@ ECHO    Core Tools     : %CORETOOLS%
 ECHO    DOC Tools      : %DOCTOOLS%
 ECHO    DB Tools       : %DBTOOLS%
 ECHO    Python Tools   : %PYTOOLS%
-ECHO    Java Tools     : $JAVATOOLS%
+ECHO    Java Tools     : %JAVATOOLS%
 ECHO.
 GOTO RUN
 
