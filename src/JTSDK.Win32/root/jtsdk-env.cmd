6b2637a490056f07867fc7fd2dc9b7d742ac297b
ki7mt <ki7mt@yahoo.com>
2018-07-30 05:13:14 -0600

    Fix typo in unix path check

--- src/JTSDK.Win32/root/jtsdk-env.cmd
+++ src/JTSDK.Win32/root/jtsdk-env.cmd
@@ -213,7 +213,7 @@ SET "svn_dir_f=%svn_dir:\=/%"
 SET JTSDK_PATH=%JTSDK_PATH%;%svn_dir%
 
 :: UNIX TOOLS
-IF EXIST "%JTSDK_CONFIG%\unix"
+IF EXIST "%JTSDK_CONFIG%\unix" (
 SET unix_dir=%JTSDK_HOME%\tools\msys2\usr\bin
 SET JTSDK_PATH=%JTSDK_PATH%;%unix_dir%
 )
